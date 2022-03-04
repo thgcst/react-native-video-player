951b1717f31ccd3672c72ef99400147afe37a8ce
Thiago Costa <thiagodias2708@gmail.com>
2022-03-04 11:40:46 -0300

    feat: delete components

--- src/components/Button/index.ts
+++
@@ -1,8 +0,0 @@
-import PrimaryButton from './Primary';
-
-export default PrimaryButton;
-
-export { default as PrimaryButton } from './Primary';
-export { default as SecondaryButton } from './Secondary';
-export { default as TertiaryButton } from './Tertiary';
-export { default as QuaternaryButton } from './Quaternary';
--- src/components/TextInput/index.tsx
+++
@@ -1,6 +0,0 @@
-import Main from './Main';
-
-export default Main;
-
-export { default as PasswordInput } from './Password';
-export { default as MaskedInput } from './Masked';
